e20e7a8ff5ec07a9b60169916a82820552c1b3e7
William Chen <wytchen90s@gmail.com>
2025-01-25 22:42:27 -0800

    Moving GolfHole Script
    cleaning up

---
+++ GolfGame/Assets/William/Scripts/Mole.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class Mole : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetDiggingPoints()
+    { 
+    
+    }
+    public void DiggingPrepRoute()
+    { 
+        
+    }
+
+    public void StartDigging()
+    { }
+}
