627c0a8bfb53b60e15911142cda65a07c12c7022
William Chen <wytchen90s@gmail.com>
2025-01-26 18:51:54 -0800

    cleaning up code

--- GolfGame/Assets/William/Scripts/Field.cs
+++ GolfGame/Assets/William/Scripts/Field.cs
@@ -6,24 +6,20 @@ using Unity.VisualScripting;
 
 public class Field : Singleton<Field>
 {
-    private List<GroundTiles> tileList = new List<GroundTiles>();
+    private List<GameObject> tileList = new List<GameObject>();
     public Vector3Int forTestingOnly;
 
-    public void AddTiles(GroundTiles tileToAdd)
+    public void AddTiles(GameObject tileToAdd)
     {
         tileList.Add(tileToAdd);
     }
 
     public void BreakGrid(Vector3Int cellPosition)
     {
-        /*foreach (GroundTiles tileToBreak in tileList)
-        {
-            tileToBreak.SeparateTiles(cellPosition);
-        }*/
-        Debug.Log("this should be 1" + tileList.Count);
+        Debug.Log(tileList.Count);
         for (int i = 0; i < tileList.Count; i++)
         {
-            tileList[i].SeparateTiles(cellPosition);
+           tileList[i].GetComponent<GroundTiles>().StartTest(cellPosition);
         }
     }
 
