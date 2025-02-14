0a3ae9ff28188f97b065483839cc8a174d39d3a1
D33P12 <deepanshu120100@gmail.com>
2025-02-14 04:32:26 -0700

    Pause menu Final Fix

--- GolfGame/Assets/Sayed/PauseMenuScript.cs
+++ GolfGame/Assets/Sayed/PauseMenuScript.cs
@@ -9,8 +9,10 @@ public class PauseMenuScript : MonoBehaviour
     void Start()
     {
         PauseMenu.SetActive(false);
+       
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
     }
-
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
@@ -25,36 +27,42 @@ public class PauseMenuScript : MonoBehaviour
             }
         }
     }
-
     public void PauseGame()
     {
         PauseMenu.SetActive(true);
-        Time.timeScale = 0f;
+        Time.timeScale = 0f; 
         isPaused = true;
-    }
 
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
     public void ResumeGame()
     {
         PauseMenu.SetActive(false);
         Time.timeScale = 1f;
         isPaused = false;
+
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
     }
     public void RestartGame()
     {
         Time.timeScale = 1f; 
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
     }
     public void GoToMainMenu()
     {
-        Time.timeScale = 1f;
+        Time.timeScale = 1f; 
         SceneManager.LoadScene(0);
-    }
 
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
     public void QuitGame()
     {
-#if UNITY_EDITOR
-        UnityEditor.EditorApplication.isPlaying = false;
-#endif
         Application.Quit();
     }
 }
